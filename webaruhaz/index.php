643101477202f9a00f515bc501288da0fe87d568
Liktor Adrián <liktora@kkszki.hu>
2021-11-23 08:35:45 +0100

    webaruhaz

---
+++ webaruhaz/index.php
@@ -0,0 +1,27 @@
+<html lang="hu">
+<?php include 'head.php';?>
+<body>
+<?php include 'top.php';?>
+<?php include 'navbar.php';?>
+<hr>
+
+<div class="container" style="margin-top:30px">
+<h1  class="text-center" > ÜDVÖZÖLLEK A WEBÁRUHÁZAMBAN!</h1>
+<hr>
+</div>
+
+<?php include 'slider.php';?>
+
+<div class="container" align="center">
+  <hr>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+<br>
+</div>
+<?php include 'footer.php';?>
+</body>
+</html>
---
+++ webaruhaz/kapcsolat.php
@@ -0,0 +1,6 @@
+<?php
+$servername = "localhost";
+$username = "webaruhaz_la";
+$password = "A123456789a";
+$dbname = "webaruhaz_la";
+?>
